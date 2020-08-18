0fe2f146d3749889eeba2fa71ec649b3ca628c10
Facundo Panizza <facundo@panizza.dev>
2020-08-18 19:17:16 -0300

    Modified the searchbar component, change the color of the input and added search when input change with useEffect

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,28 +1,29 @@
-import React, { useState, FormEvent, ChangeEvent } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import { getCharacters, clearCharacters } from '../redux/charsDuck';
 
 const SearchBar = (props: any) => {
   const [input, setInput] = useState('');
+  const [debouncedInput, setDebouncedInput] = useState(input);
 
-  const onSubmit = (e?: FormEvent<HTMLFormElement>) => {
-    if (e) {
-      e.preventDefault();
-    }
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedInput(input);
+    }, 1000);
 
-    if (input.length < 2) {
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [input]);
+
+  useEffect(() => {
+    if (debouncedInput.length < 3) {
       return;
     }
 
-    props.getCharacters(input);
-  };
-
-  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value);
-
-    onSubmit();
-  };
+    props.getCharacters(debouncedInput);
+  }, [debouncedInput, props]);
 
   const resetInput = () => {
     setInput('');
@@ -32,38 +33,17 @@ const SearchBar = (props: any) => {
 
   return (
     <>
-      <button
-        onClick={(e) => onSubmit()}
-        className="self-center h-10 rounded-l-full bg-white flex">
-        <svg
-          className="ml-2 self-center"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
-            stroke="#4A5568"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </button>
-      <form onSubmit={(e) => onSubmit(e)} className="w-full self-center">
-        <input
-          value={input}
-          onChange={(e) => onChange(e)}
-          type="text"
-          className="h-10 w-full px-2 min-w-0"
-        />
-      </form>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        type="text"
+        className="h-10 self-center w-full px-2 min-w-0 rounded-l-full bg-gray-900"
+      />
       <button
         onClick={resetInput}
-        className="self-center h-10 rounded-r-full bg-white flex">
+        className="self-center h-10 rounded-r-full bg-gray-900 flex">
         <svg
-          className="mr-2 self-center"
+          className="mr-2 self-center text-gray-400"
           width="24"
           height="24"
           viewBox="0 0 24 24"
@@ -71,7 +51,7 @@ const SearchBar = (props: any) => {
           xmlns="http://www.w3.org/2000/svg">
           <path
             d="M6 18L18 6M6 6L18 18"
-            stroke="#4A5568"
+            stroke="currentColor"
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
